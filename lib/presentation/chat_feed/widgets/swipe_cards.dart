83fe1f657c63053bd4e7fd3de203fbf40452f0fe
Matthew <matthew.rl.trent@gmail.com>
2023-03-04 01:00:01 -0800

    translation and rotation animations working for card drags

--- lib/presentation/chat_feed/widgets/swipe_cards.dart
+++ lib/presentation/chat_feed/widgets/swipe_cards.dart
@@ -1,5 +1,8 @@
+import 'package:Confessi/core/utils/numbers/number_until_limit.dart';
 import 'package:Confessi/core/utils/sizing/width_fraction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import "dart:math" as math;
 
 class SwipeCardsController extends ChangeNotifier {}
 
@@ -23,53 +26,86 @@ class _SwipeCardsState extends State<SwipeCards> {
   Widget build(BuildContext context) {
     return Stack(
       children: const [
-        _Card(),
+        _DraggableWrapper(),
       ],
     );
   }
 }
 
-class _Card extends StatefulWidget {
-  const _Card({super.key});
+class _DraggableWrapper extends StatefulWidget {
+  const _DraggableWrapper({super.key});
 
   @override
-  State<_Card> createState() => _CardState();
+  State<_DraggableWrapper> createState() => _DraggableWrapperState();
 }
 
-class _CardState extends State<_Card> with SingleTickerProviderStateMixin {
-  Offset offset = const Offset(0, 0);
+class _DraggableWrapperState extends State<_DraggableWrapper> with SingleTickerProviderStateMixin {
+  late Offset offset = const Offset(0, 0);
+  bool animateBackToStart = false;
+
+  late AnimationController rotationController;
+  late Animation rotationAnim;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // @override
+  // void didChangeDependencies() {
+  //   offset = Offset(widthFraction(context, 1 / 4), 0);
+  //   super.didChangeDependencies();
+  // }
+
+  double getRotation(double dx) {
+    double rotOffset = dx / 100;
+    int multiplier = rotOffset < 0 ? -1 : 1;
+    return multiplier * numberUntilLimit(rotOffset.abs(), 2) * math.pi / 16;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Draggable(
-      feedback: Container(
-        height: 300,
-        color: Colors.red,
-        width: 200,
-      ),
-      childWhenDragging: Container(),
-      onDragUpdate: (details) {
-        print(details);
-        setState(() => offset = details.delta);
-      },
-      // onDragCompleted: () => setState(() => offset = const Offset(0, 0)),
-      onDragEnd: (details) {
-        if (details.offset.dx.abs() > widthFraction(context, .1)) {
-          if (details.offset.dx > 0) {
-            // _onSwipeRight();
-            print("RIGHT");
-          } else {
-            // _onSwipeLeft();
-            print("LEFT");
-          }
-        }
-      },
-      child: Transform.scale(
-        scale: 1,
-        child: Container(
-          height: 300,
-          color: Colors.red,
-          width: 200,
+    return GestureDetector(
+      onPanStart: (details) => setState(() => animateBackToStart = false),
+      onPanUpdate: (details) =>
+          setState(() => offset = Offset(offset.dx + details.delta.dx, offset.dy + details.delta.dy)),
+      onPanEnd: (details) => setState(() {
+        offset = const Offset(0, 0);
+        animateBackToStart = true;
+        HapticFeedback.lightImpact();
+      }),
+      child: Container(
+        color: Colors.blue,
+        height: 400,
+        width: widthFraction(context, .8),
+        child: Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.center,
+          children: [
+            AnimatedPositioned(
+              duration: animateBackToStart ? const Duration(milliseconds: 800) : Duration.zero,
+              curve: Curves.easeOutBack,
+              left: offset.dx,
+              // right: offset.dx,
+              top: offset.dy,
+              // bottom: offset.dy,
+              child: AnimatedContainer(
+                duration: animateBackToStart ? const Duration(milliseconds: 800) : Duration.zero,
+                transform: Matrix4.rotationZ(getRotation(offset.dx)),
+                curve: Curves.easeOutBack,
+                child: Transform.rotate(
+                  // scale: offset.dy > 100 ? 1 : 1,
+                  // angle: getRotation(offset.dx),
+                  angle: 0,
+                  child: Container(
+                    height: 400,
+                    color: Colors.red,
+                    width: widthFraction(context, .8),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
